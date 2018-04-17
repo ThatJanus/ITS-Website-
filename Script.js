f9855ca4cd7a3f9aab3fc1c1924448fa3aea5172
ThatJanus <37658801+ThatJanus@users.noreply.github.com>
2018-04-17 12:32:45 +1000

    Remove Hamburger
    Got Rid of hamburger menu + other changes

--- Script.js
+++ Script.js
@@ -35,14 +35,3 @@ $(document).ready(function() {
         $('.opaque-navbar').removeClass('opaque');
     }
 });
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-}
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
-function myFunction(x) {
-  x.classList.toggle("change");
-}
