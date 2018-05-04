e0f11d801f67971186b42851f85e5928e6a81acb
ThatJanus <37658801+ThatJanus@users.noreply.github.com>
2018-05-04 13:16:17 +1000

    Version 5.1
    Added
    - Started working on responsive design within the navbar to make the navbar chane when the screen resoultion changes, this will allow for easy access the the site on mobiles

--- Script.js
+++ Script.js
@@ -35,3 +35,12 @@ $(document).ready(function() {
         $('.opaque-navbar').removeClass('opaque');
     }
 });
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar-custom") {
+      x.className += "responsive";
+  } else {
+      x.className = "navbar-custom";
+  }
+}
